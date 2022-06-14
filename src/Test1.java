46c99ec5b6e5cfa5625f5f22732f387fde330ff8
nataekoon <smathj@naver.com>
2022-06-14 23:47:56 +0900

    first

---
+++ src/Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+    public static void Log() {
+        System.out.println("안녕하세요 오랜만에 자바를 작성합니다");
+        System.out.println("자바와 스프링을 다시 해봅시다");
+    }
+
+    public static void main(String args[]) {
+        Test1.Log();
+    }
+}
