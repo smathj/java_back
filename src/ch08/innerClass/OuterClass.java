d4f8b970a554d0c1499a4fde1b2010737d74e8c9
nataekoon <smathj@naver.com>
2022-06-28 01:42:12 +0900

    Local Class

--- src/ch08/innerClass/OuterClass.java
+++ src/ch08/innerClass/OuterClass.java
@@ -2,6 +2,38 @@ package ch08.innerClass;
 
 public class OuterClass {
 
+    // 로컬 클래스
+   public void method1() {
+       
+       // 내부 클래스는 모두 접근 제한자 사용 불가
+       // static도 불가
+       class LocalClass {
+           int d;
+           void method5() {
+               System.out.println("LocalClass : " + d);
+           }
+       }
+       
+       LocalClass inner = new LocalClass();
+       inner.d = 1004;
+       inner.method5();
+   }
+
+
+    // 정적 클래스
+    static class StaticClass {
+        int b;
+        static int c;
+        void method3() {
+            System.out.println("Static Class : " + b);
+        }
+        static void method4() {
+            System.out.println("Static Class : " + c);
+        }
+
+    }
+
+    // 인스턴스 클래스
     class InstanceClass {
         int a;
 
--- src/ch08/innerClass/OuterClassTest.java
+++ src/ch08/innerClass/OuterClassTest.java
@@ -3,9 +3,23 @@ package ch08.innerClass;
 public class OuterClassTest {
     public static void main(String[] args) {
 
+        // 1. 인스턴스 클래스
+        // 외부 클래스의 인스턴스가 필요하다
+//        OuterClass outer = new OuterClass();
+//        OuterClass.InstanceClass inner01 = outer.new InstanceClass();
+//        inner01.a = 123;
+//        inner01.method2();
+
+        // 2. 정적 클래스
+        // 외부 클래스의 인스턴스가 필요없다
+//        OuterClass.StaticClass inner02 = new OuterClass.StaticClass();
+//        inner02.b = 456;
+//        inner02.method3();
+//        OuterClass.StaticClass.c = 789;
+//        OuterClass.StaticClass.method4();
+
+        // 3. 로컬 클래스
         OuterClass outer = new OuterClass();
-        OuterClass.InstanceClass inner01 = outer.new InstanceClass();
-        inner01.a = 123;
-        inner01.method2();
+        outer.method1();
     }
 }
